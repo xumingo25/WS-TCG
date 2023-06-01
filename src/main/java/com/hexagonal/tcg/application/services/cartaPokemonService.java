2f3b8be828adefb23418a9bc5afb8af2bc2631a9
Domingo <domingo.saavedra@tsoftglobal.com>
2023-05-31 23:05:53 -0400

    Se actualiza repo. Se crean clases en componente application.

---
+++ src/main/java/com/hexagonal/tcg/application/services/cartaPokemonService.java
@@ -0,0 +1,5 @@
+package com.hexagonal.tcg.application.services;
+
+public class cartaPokemonService {
+
+}
---
+++ src/main/java/com/hexagonal/tcg/application/usercases/DeleteCartaPokemonUseCaseImpl.java
@@ -0,0 +1,4 @@
+package com.hexagonal.tcg.application.usercases;
+
+public class DeleteCartaPokemonUseCaseImpl {
+}
