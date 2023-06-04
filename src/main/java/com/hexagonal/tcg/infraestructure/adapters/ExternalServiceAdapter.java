b370ae44959ad5283cffb233af10e827851f3259
Domingo <domingo.saavedra@tsoftglobal.com>
2023-06-04 09:59:47 -0400

    Se actualiza repo. No se toca componente ExternalServiceAdapter. N/A para nuestro servicio aun...

--- src/main/java/com/hexagonal/tcg/infraestructure/adapters/ExternalServiceAdapter.java
+++ src/main/java/com/hexagonal/tcg/infraestructure/adapters/ExternalServiceAdapter.java
@@ -1,4 +1,7 @@
 package com.hexagonal.tcg.infraestructure.adapters;
 
 public class ExternalServiceAdapter {
+
+    //https://jsonplaceholder.typicode.com/todos
+    //https://jsonplaceholder.typicode.com/users
 }
