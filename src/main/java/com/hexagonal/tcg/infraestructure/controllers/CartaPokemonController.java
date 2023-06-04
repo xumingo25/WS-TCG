9eef36850ff04f5330eb05f506476efb9cd6dd31
Domingo <domingo.saavedra@tsoftglobal.com>
2023-06-04 10:29:22 -0400

    Se actualiza repo. Se crean mas metodos en clase controller (getCartaById,getAllCards,updateCard,deleteCardById)

--- src/main/java/com/hexagonal/tcg/infraestructure/controllers/CartaPokemonController.java
+++ src/main/java/com/hexagonal/tcg/infraestructure/controllers/CartaPokemonController.java
@@ -4,10 +4,9 @@ import com.hexagonal.tcg.application.services.CartaPokemonService;
 import com.hexagonal.tcg.domain.models.CartaPokemon;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/cards")
@@ -25,4 +24,36 @@ public class CartaPokemonController {
         CartaPokemon createdCartaPokemon = cartaPokemonService.createCartaPokemon(carta);
         return new ResponseEntity<>(createdCartaPokemon, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{cartaId}")
+    public ResponseEntity<CartaPokemon> getCartaByCodigo(@PathVariable long cartaId){
+        return cartaPokemonService.getCartaPokemon(cartaId).map(
+                cartaPokemon -> new ResponseEntity<>(cartaPokemon, HttpStatus.OK)
+        ).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CartaPokemon>> getAllCartas(){
+        List<CartaPokemon> cartas = cartaPokemonService.getAllCartas();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @PutMapping("/{cartaId}")
+    public ResponseEntity<CartaPokemon> updateCard(@PathVariable Long cartaId, @RequestBody CartaPokemon updateCarta){
+        return cartaPokemonService.updateCartaPokemon(cartaId,updateCarta)
+                .map(cartaPokemon -> new ResponseEntity<>(cartaPokemon, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{cartaId}")
+    public ResponseEntity<CartaPokemon> deleteCardById(@PathVariable Long cartaId){
+         if(cartaPokemonService.deleteCartaPokemon(cartaId)){
+             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+         }else{
+             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+         }
+    }
+
+
 }
